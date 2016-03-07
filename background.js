c06e8b28254636a7507793a5e2a3f734133b0523
Anna <annayen2008@gmail.com>
2016-03-07 16:14:56 -0500

    add hide locked questions option

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
 
-$('.header-ac').hide();
+$(document).ready(function () {
+   
+	$('.header-ac').hide();
 
-$('.header-level').hide();
+	$('.header-level').hide();
 
-$("#problemList td:nth-child(4)").hide();
-$("#problemList td:nth-child(5)").hide();
+	$("#problemList td:nth-child(4)").hide();
+	$("#problemList td:nth-child(5)").hide();
 
+	chrome.storage.sync.get('hideLocked', function(response) {
+		if (response.hideLocked)
+			$('tr i').parent().parent().remove();
+	});
 
-$('.text-info').hide();
+	$('.text-info').hide();
+
+});
---
+++ options.js
@@ -0,0 +1,22 @@
+function save_options() {
+  var hideLocked = document.getElementById('hide-locked').checked;
+  chrome.storage.sync.set({
+    hideLocked: hideLocked
+  }, function() {
+    window.close();
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    hideLocked: true
+  }, function(items) {
+    document.getElementById('hide-locked').checked = items.hideLocked;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
