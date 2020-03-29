2b290ce6152394ed4086e1d1fbcf238272feb8ec
bingjingc <bingjingc@synology.com>
2020-03-29 12:55:45 +0800

    update

--- background.js
+++ background.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
 	var sidebarLoaded = false; // Avoid sidebar to be watched twice
 
+	$.get( "/api/problems/algorithms/", function( data ) {
+		var ac = 0;
+		var total = 0;
+		data = JSON.parse(data);
+		
+		for (var i in data.stat_status_pairs) {
+			// console.log(JSON.stringify(data.stat_status_pairs[i][j]));
+			if (!data.stat_status_pairs[i].paid_only) {
+				total += 1;
+				if (data.stat_status_pairs[i].status == "ac")
+					ac += 1;
+			}
+		}
+
+		console.log("total = " + total + " ac = " + ac);
+		$('span.label.label-primary.round').html('<span>' + ac + '/' + total+ ' Solved</span>');
+		$('div.col-xs-4.text-primary.text-center').html('<div class="text-500 status">' + (total - ac)+ ' </div><span class="text text-sm status">Todo</span>');
+		$('div.col-xs-4.text-success.text-center').html('<div class="text-500 status">' + ac + '/' + total+ ' </div><span class="text text-sm status">Solved</span>');
+		//$('spanlabel.label-primary.round').empty();
+		
+	});
+
 	// Hide main content while loading
 	$('.content-wrapper > *').not('.navbar').css('opacity', '0');
 
@@ -9,6 +31,8 @@ $(document).ready(function () {
 		properties: 'prop_innerHTML',
 		watchChildren: true,
 		callback: function (data, i) {
+			$('tr div.frequency-locked i.fa.fa-lock').closest('i').remove()
+
 			// Hide locked
 			chrome.storage.sync.get('hideLocked', function(response) {
 				if (response.hideLocked) {
